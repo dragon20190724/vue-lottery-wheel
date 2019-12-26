66f0cc8bf3237a80215440a68361d5e0468b3fee
dragon20190724 <dragon20190724@gmail.com>
2019-12-26 17:59:40 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
   devServer: { // 设置代理
     hot: true, //热加载
     host: '0.0.0.0', //ip地址
-    port: 9696, //端口
+    port: 9000, //端口
     https: false, //false关闭https，true为开启
-    open: false, //自动打开浏览器
+    open: true, //自动打开浏览器
   }
 }
